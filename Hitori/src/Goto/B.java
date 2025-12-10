73a3700dcefbd2ab6d9c3b8e5b93b5f953c29805
BaeDaeng <justin0916@naver.com>
2025-12-10 09:23:06 +0900

    Initial version

---
+++ Hitori/src/Goto/B.java
@@ -0,0 +1,7 @@
+package Goto;
+
+public class B {
+	public static void main(String[] args) {
+		System.out.println("This is Banana!!");
+	}
+}
---
+++ Hitori/src/Goto/C.java
@@ -0,0 +1,7 @@
+package Goto;
+
+public class C {
+	public static void main(String[] args) {
+		System.out.println("This is Cat!!");
+	}
+}
